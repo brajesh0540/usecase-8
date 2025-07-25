513c2a17bb26d52ff29eabf155335cc8dc7122b6
brajesh0540 <brajesh540@gmail.com>
2025-07-25 23:04:29 +0530

    alb rule modified

--- uc-8/modules/alb/main.tf
+++ uc-8/modules/alb/main.tf
@@ -66,7 +66,7 @@ resource "aws_lb_listener" "alb_listner" {
 }
 
 resource "aws_lb_listener_rule" "patients" {
-    listener_arn = aws_lb_listener_rule.http.arn
+    listener_arn = aws_lb_listener.http.arn
     priority     = 10
 
     action {
--- uc-8/modules/vpc/main.tf
+++ uc-8/modules/vpc/main.tf
@@ -16,7 +16,7 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
+  allocation_id = aws_eip.eip_nat.id
   subnet_id = aws_subnet.public_subnet[0].id
   
 }
