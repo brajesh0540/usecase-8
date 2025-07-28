9595a67d362e770364338f67fbbdfec9654bc646
brajesh0540 <brajesh540@gmail.com>
2025-07-28 11:47:14 +0530

    commit

--- uc-8/modules/vpc/main.tf
+++ uc-8/modules/vpc/main.tf
@@ -83,7 +83,7 @@ resource "aws_route_table_association" "public" {
 
 resource "aws_route_table_association" "private" {
   count = length(aws_subnet.private_subnet)
-  subnet_id = aws_subnet.private[count.index]
+  subnet_id = aws_subnet.private_subnet[count.index].id
   route_table_id = aws_route_table.private.id
   
 }
