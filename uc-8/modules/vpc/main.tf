3c9a0027b686025adf6d43aec9f5f0a2323f4912
brajesh0540 <brajesh540@gmail.com>
2025-07-28 11:35:22 +0530

    commit

--- uc-8/modules/vpc/main.tf
+++ uc-8/modules/vpc/main.tf
@@ -47,6 +47,44 @@ resource "aws_subnet" "private_subnet" {
   
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${var.vpc_name}-public-route-table"
+  }
+  
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = {
+    Name = "${var.vpc_name}-private-route-table"
+  }
+  
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public_subnet)
+  subnet_id = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public.id
+  
+}
+
+resource "aws_route_table_association" "private" {
+  count = length(aws_subnet.private_subnet)
+  subnet_id = aws_subnet.private[count.index]
+  route_table_id = aws_route_table.private.id
+  
+}
+
 data "aws_availability_zones" "available" {
   state = "available"
 }
