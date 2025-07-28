bb21b2efe1c6d29d62f215d86c123a25c586e899
brajesh0540 <brajesh540@gmail.com>
2025-07-28 11:09:50 +0530

    commit

--- uc-8/modules/ecs/main.tf
+++ uc-8/modules/ecs/main.tf
@@ -2,6 +2,29 @@ resource "aws_ecs_cluster" "ecs_cluster" {
   name = var.cluster_name
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
 resource "aws_ecs_task_definition" "task" {
   for_each = var.services
   family = "${var.cluster_name}-${each.key}"
@@ -9,8 +32,8 @@ resource "aws_ecs_task_definition" "task" {
   requires_compatibilities = ["FARGATE"]
   cpu = each.value.cpu
   memory = each.value.memory
-  execution_role_arn = var.execution_role_arn
-  task_role_arn = var.task_arn_role
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn = null
 
   container_definitions = jsonencode([
     {
